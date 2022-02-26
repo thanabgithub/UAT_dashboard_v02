5201445089c8170cfa613c5eb5a9800b5bf14976
thanabgithub <thana.b@outlook.jp>
2022-02-27 04:06:40 +0700

    new API due to change project scope

--- backend/preload.py
+++ backend/preload.py
@@ -7,46 +7,15 @@ from apscheduler.triggers.interval import IntervalTrigger
 scheduler = BlockingScheduler()
 
 
-@scheduler.scheduled_job(IntervalTrigger(hours=6))
+@scheduler.scheduled_job(IntervalTrigger(hours=3))
 def execute_scheduled_preload_routine():
-    print("get_woeid_trends")
-    woeid_trends = get_woeid_trends()
-    meta = prepare_meta_data()
-
-    (
-        region_to_rank_to_keyword,
-        rank_to_keyword_agg,
-        keyword_to_region_to_rank,
-        keyword_to_rank_agg,
-        region_to_unqiue_keyword,
-    ) = init_preload_data(meta)
-    print("process_rank_data")
-    (
-        region_to_rank_to_keyword,
-        rank_to_keyword_agg,
-        keyword_to_region_to_rank,
-        keyword_to_rank_agg,
-        region_to_unqiue_keyword,
-    ) = process_rank_data(woeid_trends,
-        region_to_rank_to_keyword,
-        rank_to_keyword_agg,
-        keyword_to_region_to_rank,
-        keyword_to_rank_agg,
-        region_to_unqiue_keyword,
-    )
-    print("add_img_url")
-    keyword_to_rank_agg = add_img_url(keyword_to_rank_agg)
-    print("save_file")
-    save_file(
-        region_to_rank_to_keyword,
-        rank_to_keyword_agg,
-        keyword_to_region_to_rank,
-        keyword_to_rank_agg,
-        region_to_unqiue_keyword,
-    )
+    print("start execute_scheduled_preload_routine ")
+    regional_df, national_df = process_data()
+    regional_df, national_df = prepare_url(regional_df, national_df)
+    prepare_meta_n_dump(regional_df, national_df)
     print('success')
     
 if __name__ == "__main__":
-    print('start preload')
+    print('start main')
     execute_scheduled_preload_routine()
     scheduler.start()
